fd5c49929cb78610bf6f36f0d8dd5f537ee814f0
molika-thai <molika.thai@hetic.net>
2018-01-16 18:54:02 +0100

    init front end fidmi app
    start first card component

---
+++ front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-ons-page id="app">
+    <home-page></home-page>
+  </v-ons-page>
+</template>
+
+<script>
+import HomePage from './components/HomePage'
+
+export default {
+  name: 'app',
+  data () {
+    {}
+  },
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ons-splitter-side[side=left][animation=overlay] {
+  border-right: 1px solid #BBB;
+}
+</style>
---
+++ front/src/components/HomePage.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-ons-page>
+    <v-ons-toolbar class="home-toolbar">
+      <div class="center">{{ msg }}</div>
+    </v-ons-toolbar>
+
+    <v-ons-list-title>Vue title</v-ons-list-title>
+    <v-ons-list class="square-cards cards-container">
+      <v-ons-list-item v-for="meal in meals" :key="meal.link">
+        <v-ons-card>
+          <div class="title">
+            Le {{ meal.date }}, c'est {{ meal.mealName }} avec {{ meal.hostSurname }}
+          </div>
+          <div class="content">
+            Bonjour
+          </div>
+        </v-ons-card>
+      </v-ons-list-item>
+    </v-ons-list>
+
+    <v-ons-list-title>Vue.js Ecosystem</v-ons-list-title>
+    <v-ons-row>
+      <v-ons-col>
+        <v-ons-card @click="goTo('http://router.vuejs.org/')">vue-router</v-ons-card>
+      </v-ons-col>
+      <v-ons-col>
+        <v-ons-card @click="goTo('http://vuex.vuejs.org/')">vuex</v-ons-card>
+      </v-ons-col>
+    </v-ons-row>
+    <v-ons-row>
+      <v-ons-col>
+        <v-ons-card @click="goTo('http://vue-loader.vuejs.org/')">vue-loader</v-ons-card>
+      </v-ons-col>
+      <v-ons-col>
+        <v-ons-card @click="goTo('https://github.com/vuejs/awesome-vue')">awesome-vue</v-ons-card>
+      </v-ons-col>
+    </v-ons-row>
+
+  </v-ons-page>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      msg: "Fidmi",
+      meals: [
+        {
+          hostName: "Salas",
+          hostSurname: "Olivia",
+          mealName: "blanquette de veau",
+          date: "16/01/2018",
+          hour: "19h",
+          placesLeft: 2,
+          imgUrl: ""
+        },
+        {
+          hostName: "Reininger",
+          hostSurname: "Ombeline",
+          mealName: "boeuf bourguignon",
+          date: "16/01/2018",
+          hour: "19h",
+          placesLeft: 1,
+          imgUrl: ""
+        }
+      ]
+    };
+  },
+  methods: {
+    goTo(url) {
+      window.open(url, "_blank");
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  max-width: 300px;
+}
+
+ons-list-title {
+  text-transform: none;
+}
+
+ons-list-title:not(:first-of-type) {
+  margin-top: 30px;
+}
+
+ons-card {
+  text-align: center;
+}
+
+ons-list-item,
+ons-card {
+  cursor: pointer;
+}
+</style>
