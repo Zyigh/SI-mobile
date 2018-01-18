4fbed31b7c9668d7d3714466bb6e9a7dfe91685d
molika-thai <molika.thai@hetic.net>
2018-01-18 12:36:22 +0100

    trying dymanic sections (homepage)

--- front/src/pages/Home.vue
+++ front/src/pages/Home.vue
@@ -3,7 +3,7 @@
       <head-bar></head-bar>
       <div class="page__inner">
         <section>
-          <h2 class="title--bordered">Aujourd'hui</h2>
+          <h2 class="title--bordered">{{ this.sectionTitle }}</h2>
           <ul class="list sq-list" v-if="meals !== []">
             <li class="sq-list__item" v-for="meal in meals" :key="meal.id">
               <square-card v-bind:mealdata="meal"></square-card>
@@ -25,11 +25,17 @@ export default {
   name: "Home",
   mounted: function(){
     this.getMeals();
+    this.renderSectionTitle();
+
   },
   
   data() {
     return {
       meals: [],
+      dates: [],
+      currentDate: moment(),
+      eventDate: '',
+      sectionTitle: ''
     };
   },
   
@@ -42,11 +48,31 @@ export default {
             let date =  moment.unix(data[i].date).format("DD/MM/YY");
 
             this.meals.push(data[i]);
+            this.dates.push(date);
           }
         }, response => {
           alert('Have not been able to retrieve data, please ask Hugo Medina to fix me');
         }
       );
+    },
+
+    renderSectionTitle: function(){
+      var formattedToday = this.currentDate.format('DD/MM/YY');
+      var formattedTomorrow = this.currentDate.add(1, 'd').format('DD/MM/YY');
+
+      console.log(this.dates);
+
+      for (let i = 0; i < this.dates.length; i++){
+        console.log(i);
+        if (formattedToday == this.dates[i]){
+          this.sectionTitle = 'Aujourd\'hui';
+          console.log(this.sectionTitle);
+        } else if (formattedTomorrow == this.dates[i]){
+          this.sectionTitle = 'Demain'
+        } else {
+          this.sectionTitle = 'Le ' + this.dates[i];
+        } 
+      }
     }
   },
   components: {
